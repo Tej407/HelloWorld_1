902bba7f6060002a0c5f84f38e1bf15c2f22e44a
Tej407 <63239512+Tej407@users.noreply.github.com>
2020-07-14 21:37:04 +0530

    Update and rename __mian__.py to __main__.py

--- __main__.py
+++ __main__.py
@@ -4,6 +4,9 @@ import os
 script_files=glob.glob('*.py')
 print(script_files)
 #script_files.remove(os.sys.argv[0])
-
-for file in script_files:
-    os.system('python %s'%file)
+i=1
+while i=1:
+    for file in script_files:
+        os.system('python %s'%file)
+    i+=1
+    
