0dee7793ca90d89afa8248e3e196300dfbde5f4c
Tej407 <63239512+Tej407@users.noreply.github.com>
2020-07-14 21:38:16 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -5,7 +5,7 @@ script_files=glob.glob('*.py')
 print(script_files)
 #script_files.remove(os.sys.argv[0])
 i=1
-while i=1:
+while i==1:
     for file in script_files:
         os.system('python %s'%file)
     i+=1
